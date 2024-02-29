e121117619afb9c5f6c96f1ba3caba386bfccc2d
jiangtao <410518072@qq.com>
2024-03-01 03:41:22 +0800

    merge settings

--- src/main/java/cn/moon/docker/core/log/LogController.java
+++ src/main/java/cn/moon/docker/core/log/LogController.java
@@ -20,53 +20,45 @@ public class LogController {
     @GetMapping
     public ResponseEntity<StreamingResponseBody> log(@PathVariable("logId") String logId) throws Exception {
 
+        StreamingResponseBody responseBody = os -> {
+            File file = LogConstants.getLogPath(logId);
+            PrintWriter writer = new PrintWriter(os);
+            writer.println("日志文件 " + file.getAbsolutePath());
 
-        StreamingResponseBody responseBody = new StreamingResponseBody() {
-            @Override
-            public void writeTo(OutputStream os) throws IOException {
-                File file = LogConstants.getLogPath(logId);
-                PrintWriter writer = new PrintWriter(os);
-                writer.println("日志文件 " + file.getAbsolutePath());
-
-                if (!file.exists()) {
-                    writer.write("日志文件已被清理" + file.getAbsolutePath());
-                    return;
-                }
-
-
-                RandomAccessFile accessFile = new RandomAccessFile(file, "r");
-                String line;
-
-                boolean alive= true;
-                do {
-                    while ((line = accessFile.readLine()) != null) {
-                        byte[] bytes = line.getBytes(StandardCharsets.ISO_8859_1);
-                        String lineUTF8 = new String(bytes, StandardCharsets.UTF_8);
-
-                        writer.println(lineUTF8);
-                    }
-                    writer.flush();
-
-                    try {
-                        Thread.sleep(1000 * 5);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    alive = System.currentTimeMillis() - file.lastModified() < 1000 * 60 * 5;
-                    writer.write(".");
-                }while (alive);
-
-
-                accessFile.close();
+            if (!file.exists()) {
+                writer.write("日志文件已被清理" + file.getAbsolutePath());
             }
+
+
+            RandomAccessFile accessFile = new RandomAccessFile(file, "r");
+            String line;
+
+            boolean alive= true;
+            do {
+                while ((line = accessFile.readLine()) != null) {
+                    byte[] bytes = line.getBytes(StandardCharsets.ISO_8859_1);
+                    String lineUTF8 = new String(bytes, StandardCharsets.UTF_8);
+
+                    writer.println(lineUTF8);
+                }
+                writer.flush();
+
+                try {
+                    Thread.sleep(1000 * 5);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                alive = System.currentTimeMillis() - file.lastModified() < 1000 * 60 * 5;
+                writer.write(".");
+            }while (alive);
+
+
+            accessFile.close();
         };
         // 响应到客户端
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
                 .body(responseBody);
-
-
-
     }
 
 
